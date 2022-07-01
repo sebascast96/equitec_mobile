b57ac79a09d591a365c6a46283213728d3de48d0
sebasCast1996 <105011615+sebasCast1996@users.noreply.github.com>
2022-07-01 18:26:52 -0500

    Login

--- src/screens/Login/Login.component.jsx
+++ src/screens/Login/Login.component.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import styles from './styles';
-import {View, Text} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import Button from '../../components/Button'
-import { Constants } from '../../common';
+import React from "react";
+import styles from "./styles";
+import { View, Text } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Button from "../../components/Button";
+import { Constants } from "../../common";
 
-const LoginComponent = props => {
-  const {handlePressNext} = props;
+const LoginComponent = (props) => {
+  const { handlePressNext } = props;
   return (
     <SafeAreaProvider>
       <View style={styles.loginScreen}>
         <View style={styles.loginContainer}>
-            <Text>Login</Text>
-            <Button 
-					    handlePress={handlePressNext}
-              buttonText={Constants.language.generic.next}
-            />
+          <Text>Login</Text>
+
+          <Button
+            handlePress={handlePressNext}
+            buttonText={Constants.language.generic.next}
+          />
         </View>
       </View>
     </SafeAreaProvider>
