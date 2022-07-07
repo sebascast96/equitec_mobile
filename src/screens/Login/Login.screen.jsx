08f5b280b72e16a7bec3d91eab2ae3a2c6a2f8b5
sebasCast1996 <105011615+sebasCast1996@users.noreply.github.com>
2022-07-07 18:49:08 -0500

    login

--- src/screens/Login/Login.screen.jsx
+++ src/screens/Login/Login.screen.jsx
@@ -4,11 +4,18 @@ import styles from "./styles";
 import LoginComponent from "./Login.component";
 import { useNavigation } from "@react-navigation/native";
 import { Constants } from "../../common";
+import { login } from "../../client/index";
 const LoginScreen = (props) => {
   const navigation = useNavigation();
 
-  const handlePressNext = () => {
-    navigation.navigate(Constants.screens.Home);
+  const handlePressNext = async () => {
+    const res = await login("tecnico.prueba@equitec.com.co", "123456");
+    console.log(res);
+    if (res.data == "") {
+      console.log("nel");
+    } else {
+      navigation.navigate(Constants.screens.Home);
+    }
   };
   return <LoginComponent handlePressNext={handlePressNext} />;
 };
